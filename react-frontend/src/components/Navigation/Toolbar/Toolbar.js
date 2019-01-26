7fdc964544fbf0da62f8f732782a1bf3b58e89d6
Shivam Rastogi <shivam.ras304@gmail.com>
2019-01-26 15:36:56 +0530

    FE: Added styling back to Auth buttons

--- react-frontend/src/components/Navigation/Toolbar/Toolbar.js
+++ react-frontend/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,25 +2,31 @@ import React from "react";
 import styles from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import { Link } from 'react-router-dom'
+import Button from '../../UI/Button/Button'
 
 const toolbar = () => (
   <header className={styles.Toolbar}>
     <div className={styles.Logo}>
       <Logo />
     </div>
-    <div className={styles.AuthManager}>
-      <Link to={{
+    <div>
+      <Button btnType='Normal' className={styles.AuthButtons}>
+        <Link to={{
           pathname: '/auth', 
           state: {
             isLoggingIn: true
           }
         }}>Log In</Link>
-      <Link to={{
+      </Button>
+      <div className={styles.HorizontalStub}></div>
+      <Button btnType='Accent' className={styles.AuthButtons}>
+        <Link to={{
           pathname: '/auth', 
           state: {
             isLoggingIn: false
           }
         }}>Sign up</Link>
+      </Button>
     </div>
   </header>
 );
